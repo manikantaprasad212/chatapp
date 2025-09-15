1bf5d41aceffed3f53eae7b9dabf8b66c91a6dd5
P.D.M.Prasad <manikantaprasadprasadula@gmail.com>
2025-09-15 23:30:33 +0530

    signup Page implemented on frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
 import { ENV } from "./lib/env.js";
 
@@ -17,6 +18,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); //req.body
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
 app.use(cookieParser());
 
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,12 +1,37 @@
 import {create} from "zustand";
+import { axiosInstance} from '../lib/axios'
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-    authUser: {name: "john",_id:123, age:24},
-    isLoggedIn:false,
+   authUser: null,
+   isCheckingAuth:true,
+   isSigningUp: false,
 
-    login: () => {
-        console.log("we just Logged in");
-        set({isLoggedIn:true, isLoading:true})
-    },
+   checkAuth : async () => {
+    try {
+        const res = await axiosInstance.get("/auth/check")
+        set({authUser: res.data})
+    } catch (error) {
+        console.log("Error in authcheck", error)
+        set({authUser:null})
+    } finally {
+        set({isCheckingAuth:false})
+    }
+   },
+
+   signup: async (data) => {
+    set({isSigningUp:true})
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser: res.data});
+
+            toast.success("Account created successfully")
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+    set({isSigningUp:false})
+        }
+   }
+
 }));
 
